60063160ab4f6d6785c63044b42aeb7eff2bc057
GitHub Action <action@github.com>
2021-05-27 09:32:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/jyno12/daily/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jyno12/daily/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jyno12.daily.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jyno12.daily.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/jyno12/daily/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jyno12/daily/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jyno12.daily.services
 
+import com.github.jyno12.daily.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
